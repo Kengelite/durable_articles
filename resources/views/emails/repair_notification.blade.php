67ae7cdc28e0bbb39afbc09cf960165dfec054f9
mynameisrattaket <rattaket.w@kkumail.com>
2025-05-21 13:29:58 +0700

    เเก้เมล

--- resources/views/emails/repair_notification.blade.php
+++ resources/views/emails/repair_notification.blade.php
@@ -83,7 +83,7 @@
         </div>
 
         <div class="footer">
-            <p>หากคุณมีข้อสงสัยเพิ่มเติม กรุณาติดต่อเราที่ <a href="mailto:support@example.com">computing.kku@kku.ac.th</a></p>
+            <p>หากคุณมีข้อสงสัยเพิ่มเติม กรุณาติดต่อเราที่ <a href="mailto:support@example.com">repaircpkku@gmail.com</a></p>
         </div>
     </div>
 </body>
